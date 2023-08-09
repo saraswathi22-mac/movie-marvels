f7eece9c43854db6662d6b25f12fa22b91bffeda
exit <saraswathi.mac@gmail.com>
2023-08-09 15:08:27 +0530

    Implemented react router in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <>MovieMarvels</>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
