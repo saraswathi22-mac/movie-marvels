d15dd88ddcefc9013f45394292c1876c377fbda6
exit <saraswathi.mac@gmail.com>
2023-08-09 15:25:38 +0530

    implemented page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
   );
