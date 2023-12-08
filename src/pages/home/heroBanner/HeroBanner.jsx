bb3116693986c09781ee8220e581a84947c15004
exit <saraswathi.mac@gmail.com>
2023-12-08 13:46:14 +0530

    Updated text change

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -39,7 +39,7 @@ const HeroBanner = () => {
         <div className="heroBannerContent">
           <span className="title">Welcome.</span>
           <span className="subTitle">
-            Millions of movies, TV shows and people to discover. Explore now.
+            Millions of Movies, TV shows and people to discover. Explore now.
           </span>
           <div className="searchInput">
             <input
