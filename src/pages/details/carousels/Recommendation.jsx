573628857246e93a999c6d81044b56b85876c8e4
exit <saraswathi.mac@gmail.com>
2026-02-27 21:28:55 +0530

    Updated recommendations

--- src/pages/details/carousels/Recommendation.jsx
+++ src/pages/details/carousels/Recommendation.jsx
@@ -3,14 +3,20 @@ import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
 const Recommendation = ({ mediaType, id }) => {
-  const { data, loading, error } = useFetch(
+  const { data, loading } = useFetch(
     `/${mediaType}/${id}/recommendations`
   );
 
+  const results = data?.results || [];
+
+  // Don't render section if no recommendations
+  if (!loading && results.length === 0) {
+    return null;
+  }
+
   return (
     <Carousel
-      title="Recommendations"
-      data={data?.results}
+      data={results}
       loading={loading}
       endpoint={mediaType}
     />
