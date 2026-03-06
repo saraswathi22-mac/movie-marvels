974d7293b118cc22b9d0f5e72ccc4f383f5bd48a
exit <saraswathi.mac@gmail.com>
2026-03-06 19:59:57 +0530

    Updated Search results

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 import "./style.scss";
@@ -6,42 +6,38 @@ import { fetchDataFromApi } from "../../utils/api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import MovieCard from "../../components/movieCard/MovieCard";
 import Spinner from "../../components/spinner/Spinner";
-// import noResults from "../../assets/no-results.png";
 
 const SearchResult = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [pageNum, setPageNum] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const [loading, setLoading] = useState(false);
+
   const { query } = useParams();
 
-  const fetchInitialData = () => {
+  const fetchInitialData = async () => {
     setLoading(true);
-    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
-      (res) => {
-        setData(res);
-        setPageNum((prev) => prev + 1);
-        setLoading(false);
-      }
-    );
+
+    const res = await fetchDataFromApi(`/search/multi?query=${query}&page=1`);
+
+    setData(res?.results || []);
+    setPageNum(2);
+    setTotalPages(res?.total_pages || 1);
+
+    setLoading(false);
   };
 
-  const fetchNextPageData = () => {
-    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
-      (res) => {
-        if (data?.results) {
-          setData({
-            ...data,
-            results: [...data?.results, ...res.results],
-          });
-        } else {
-          setData(res);
-        }
-        setPageNum((prev) => prev + 1);
-      }
+  const fetchNextPageData = async () => {
+    const res = await fetchDataFromApi(
+      `/search/multi?query=${query}&page=${pageNum}`
     );
+
+    setData((prev) => [...prev, ...(res?.results || [])]);
+    setPageNum((prev) => prev + 1);
   };
 
   useEffect(() => {
+    setData([]);
     setPageNum(1);
     fetchInitialData();
   }, [query]);
@@ -49,32 +45,39 @@ const SearchResult = () => {
   return (
     <div className="searchResultsPage">
       {loading && <Spinner initial={true} />}
+
       {!loading && (
         <ContentWrapper>
-          {data?.results?.length > 0 ? (
+          {data.length > 0 ? (
             <>
               <div className="pageTitle">
-                {`Search ${
-                  data?.total_results > 1 ? "results" : "result"
-                } of '${query}'`}
+                {`Search results of '${query}'`}
               </div>
+
               <InfiniteScroll
                 className="content"
-                dataLength={data?.results?.length || []}
+                dataLength={data.length}
                 next={fetchNextPageData}
-                hasMore={pageNum <= data?.total_pages}
+                hasMore={pageNum <= totalPages}
                 loader={<Spinner />}
               >
-                {data?.results.map((item, index) => {
-                  if (item.media_type === "person") return;
+                {data.map((item, index) => {
+                  if (item.media_type === "person") return null;
+
                   return (
-                    <MovieCard key={index} data={item} fromSearch={true} />
+                    <MovieCard
+                      key={`${item.id}-${index}`}
+                      data={item}
+                      fromSearch={true}
+                    />
                   );
                 })}
               </InfiniteScroll>
             </>
           ) : (
-            <span className="resultNotFound">Sorry, Results not found!</span>
+            <span className="resultNotFound">
+              Sorry, Results not found!
+            </span>
           )}
         </ContentWrapper>
       )}
